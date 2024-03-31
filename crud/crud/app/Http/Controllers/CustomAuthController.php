ccf3de042955514427c05b969cc8a7c1c7071f50
ducmanh2017vtel <ducmanh2017vtel@gmail.com>
2024-03-31 18:07:08 +0700

    tao controller dang ky

---
+++ crud/crud/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+//Unknow
+class CustomAuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.registration');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'phone' => 'required',
+            'img' => 'required',
+        ]);
+
+        $data = $request->all();
+        $check = $this->create($data);
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'img' => $data['img'],
+        ]);
+    }
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+}
