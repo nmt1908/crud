d48564f85c849093c0a6b9cdb30aed71581e6076
nmt1908 <beosex11223@gmail.com>
2024-03-31 17:02:38 +0700

    create CustomAuthController.php & add route

--- crud/crud/routes/web.php
+++ crud/crud/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CustomAuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+Route::delete('/delete-user/{id}', [CustomAuthController::class, 'deleteUser'])->name('delete.user');
+Route::get('listuser', [CustomAuthController::class, 'listuser'])->name('listuser');
 Route::get('/', function () {
     return view('welcome');
 });
