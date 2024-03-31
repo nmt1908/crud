bab229d05c0917357da3c93786f6a2336c46ed0d
nmt1908 <beosex11223@gmail.com>
2024-03-31 16:54:51 +0700

    route login

--- crud/crud/routes/web.php
+++ crud/crud/routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
 Route::get('/', function () {
     return view('welcome');
 });
