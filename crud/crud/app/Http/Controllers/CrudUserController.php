6585497cff052207da5b0f963ac9fe1cff93876c
ngduc1008 <91880752+ngduc1008@users.noreply.github.com>
2024-04-02 00:53:07 +0700

    Tao file CrudUserController de chinh sua User

---
+++ crud/crud/app/Http/Controllers/CrudUserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * CRUD User controller
+ */
+class CrudUserController extends Controller
+{
+
+    
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.update', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,id,'.$input['id'],
+            'password' => 'required|min:6',
+        ]);
+
+       $user = User::find($input['id']);
+       $user->name = $input['name'];
+       $user->email = $input['email'];
+       $user->password = Hash::make($input['password']);
+       $user->save();
+
+        return redirect("listuser")->withSuccess('You have signed-in');
+    }
+
+}
